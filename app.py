2fabb6d391e1b6212fefaacb924a7641ba008d93
meteornkr <jbvilleza@student.apc.edu.ph>
2024-10-10 23:58:50 +0800

    added files subject to change

---
+++ app.py
@@ -0,0 +1,44 @@
+from flask import Flask, request, render_template
+import numpy as np
+from pd_module import rf, main
+
+app = Flask(__name__)
+
+# Define the prediction function using your existing code
+def get_prediction_from_url(test_url):
+    features_test = main(test_url)
+    
+    # Ensure feature count matches the trained model
+    if len(features_test) != rf.n_features_in_:
+        return "Feature count mismatch."
+    
+    # Reshape and predict
+    features_test = np.array(features_test).reshape((1, -1))
+    pred = rf.predict(features_test)
+    
+    # Map the prediction to the correct category
+    if pred[0] == "benign":
+        return "SAFE"
+    elif pred[0] == "defacement":
+        return "DEFACEMENT"
+    elif pred[0] == "phishing":
+        return "PHISHING"
+    elif pred[0] == "malware":
+        return "MALWARE"
+    else:
+        return "UNKNOWN"
+
+# Route for the home page
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+# Route for handling the prediction request
+@app.route('/predict', methods=['POST'])
+def predict():
+    url = request.form['url']
+    prediction = get_prediction_from_url(url)
+    return render_template('index.html', prediction=prediction, url=url)
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ pd_module.py
@@ -0,0 +1,149 @@
+import numpy as np
+import pandas as pd
+from sklearn.metrics import classification_report,confusion_matrix, accuracy_score
+from sklearn.model_selection import train_test_split
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.ensemble import RandomForestClassifier
+from sklearn import metrics
+
+df = pd.read_csv('malicious_phish.csv')
+
+import re
+from urllib.parse import urlparse
+
+# https://www.youtube.com/watch?v=EB5FAwHqpm4
+# Check for IP address usage in URL
+def having_ip_address(url):
+    match = re.search(
+        '(([01]?\d\d?|2[0-4]\d|25[0-5])\.([01]?\d\d?|2[0-4]\d|25[0-5])\.([01]?\d\d?|2[0-4]\d|25[0-5])\.([01]?\d\d?|2[0-4]\d|25[0-5])\/)'  # IPv4
+        '((0x[0-9a-fA-F]{1,2})\.(0x[0-9a-fA-F]{1,2})\.(0x[0-9a-fA-F]{1,2})\.(0x[0-9a-fA-F]{1,2})\/)' # IPv4 in hexadecimal
+        '(?:[a-fA-F0-9]{1,4}:){7}[a-fA-F0-9]{1,4}', url)  # Ipv6
+
+    if match:
+        # print match.group()
+        return 1
+    else:
+        # print 'No matching pattern found'
+        return 0
+    
+# Check if URL contains the hostname
+def abnormal_url(url):
+    hostname = urlparse(url).hostname
+    hostname = str(hostname)
+    match = re.search(hostname, url)
+    if match:
+        # print match.group()
+        return 1
+    else:
+        # print 'No matching pattern found'
+        return 0
+
+# Applying the functions
+df['use_of_ip'] = df['url'].apply(having_ip_address)
+df['abnormal_url'] = df['url'].apply(abnormal_url)
+
+from googlesearch import search
+def google_index(url):
+    site = search(url, 5)
+    if site:
+        return 1
+    else:
+        return 0
+df['google_index'] = df['url'].apply(lambda i: google_index(i))
+
+feature = ['www','@','?','-','=','.','#','%','+','$','!','*',',','//']
+for a in feature:
+    df[a] = df['url'].apply(lambda i: i.count(a))
+
+    
+def shortening_service(url):
+    match = re.search('bit\.ly|goo\.gl|shorte\.st|go2l\.ink|x\.co|ow\.ly|t\.co|tinyurl|tr\.im|is\.gd|cli\.gs|'
+                      'yfrog\.com|migre\.me|ff\.im|tiny\.cc|url4\.eu|twit\.ac|su\.pr|twurl\.nl|snipurl\.com|'
+                      'short\.to|BudURL\.com|ping\.fm|post\.ly|Just\.as|bkite\.com|snipr\.com|fic\.kr|loopt\.us|'
+                      'doiop\.com|short\.ie|kl\.am|wp\.me|rubyurl\.com|om\.ly|to\.ly|bit\.do|t\.co|lnkd\.in|'
+                      'db\.tt|qr\.ae|adf\.ly|goo\.gl|bitly\.com|cur\.lv|tinyurl\.com|ow\.ly|bit\.ly|ity\.im|'
+                      'q\.gs|is\.gd|po\.st|bc\.vc|twitthis\.com|u\.to|j\.mp|buzurl\.com|cutt\.us|u\.bb|yourls\.org|'
+                      'x\.co|prettylinkpro\.com|scrnch\.me|filoops\.info|vzturl\.com|qr\.net|1url\.com|tweez\.me|v\.gd|'
+                      'tr\.im|link\.zip\.net',
+                      url)
+    if match:
+        return 1
+    else:
+        return 0
+
+
+df['short_url'] = df['url'].apply(lambda i: shortening_service(i))
+
+def url_length(url):
+    return len(str(url))
+
+df['url_length'] = df['url'].apply(lambda i: url_length(i))
+
+def hostname_length(url):
+    return len(urlparse(url).netloc)
+
+df['hostname_length'] = df['url'].apply(lambda i: hostname_length(i))
+
+def suspicious_words(url):
+    match = re.search('PayPal|login|signin|bank|account|update|free|lucky|service|bonus|ebayisapi|webscr',
+                      url)
+    if match:
+        return 1
+    else:
+        return 0
+df['sus_url'] = df['url'].apply(lambda i: suspicious_words(i))
+
+def fd_length(url):
+    urlpath= urlparse(url).path
+    try:
+        return len(urlpath.split('/')[1])
+    except:
+        return 0
+
+df['fd_length'] = df['url'].apply(lambda i: fd_length(i))
+
+X = df[['use_of_ip', 'abnormal_url', 'google_index', 'www', '@', '?', '-', '=', '.', '#', '%', '+', '$', '!', '*', ',', '//', 'short_url', 'url_length', 'hostname_length', 'sus_url', 'fd_length']]
+
+#Target Variable
+y = df['type']
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, stratify=y, test_size=0.2,shuffle=True, random_state=5)
+
+rf = RandomForestClassifier(n_estimators=100,max_features='sqrt')
+rf.fit(X_train,y_train)
+y_pred_rf = rf.predict(X_test)
+print(classification_report(y_test,y_pred_rf,target_names=['benign','malware', 'phishing', 'defacement']))
+
+score = metrics.accuracy_score(y_test, y_pred_rf)
+print("accuracy:   %0.3f" % score)
+
+def main(url):
+
+    status = []
+
+    status.append(having_ip_address(url))
+    status.append(abnormal_url(url))
+    status.append(google_index(url))
+    status.append(shortening_service(url))
+    status.append(url_length(url))
+    status.append(hostname_length(url))
+    status.append(suspicious_words(url))
+    status.append(fd_length(url))
+    status.append(url.count('www'))
+    status.append(url.count('@'))
+    status.append(url.count('%'))
+    status.append(url.count('-'))
+    status.append(url.count('='))
+    status.append(url.count('.'))
+    status.append(url.count('#'))
+    status.append(url.count('$'))
+    status.append(url.count('!'))
+    status.append(url.count('*'))
+    status.append(url.count(','))
+    status.append(url.count('//'))
+    status.append(url.count('https'))
+    status.append(url.count('http'))
+
+    return status
